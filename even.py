de62bc15870c339f11f992fb065c0220e6eab32e
srikanthpragada <srikanthpragada@gmail.com>
2019-06-24 09:10:48 +0530

    24-JUNE

---
+++ even.py
@@ -0,0 +1,9 @@
+# program to print whether a number is even or not
+
+n = int(input("Enter a number : "))
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
---
+++ numbers.py
@@ -0,0 +1,9 @@
+# Program to print numbers from 1 to 10
+
+n = 1
+while n <= 10:
+    print(n)
+    n += 1
+
+for n in range(1, 11):
+    print(n)
