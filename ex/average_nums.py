0a1c67f46f4b6a2c3da87c311c9082cc5080eb51
srikanthpragada <srikanthpragada@gmail.com>
2019-06-25 07:57:46 +0530

    25-JUNE

---
+++ ex/average_nums.py
@@ -0,0 +1,10 @@
+
+# Average of 10 numbers
+
+sum = 0
+for i in range(1,6):
+    num  = int(input("Enter a number :"))
+    sum += num
+
+print("Average = ", sum / 5)
+
---
+++ ex/factors.py
@@ -0,0 +1,7 @@
+# Program to print factors of the given number
+
+num = int(input("Enter a number :"))
+for i in range(2, num//2 + 1):
+    if num % i == 0:
+        print(i)
+
