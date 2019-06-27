5d9939648e2f4d9a07ac090bef36d0262eb80e58
srikanthpragada <srikanthpragada@gmail.com>
2019-06-27 07:59:34 +0530

    27-JUNE

---
+++ ex/count_words.py
@@ -0,0 +1,4 @@
+
+st = input("Enter a string :")
+
+print( "No. of words : ", st.count(' ') + 1)
---
+++ ex/sum_of_asciii.py
@@ -0,0 +1,6 @@
+st = input("Enter a string : ")
+sum = 0
+for c in st:
+    sum += ord(c)
+
+print(f"Sum = {sum}")
