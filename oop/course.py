b5cf2bebf90344a408adee0d6013a338e6939329
srikanthpragada <srikanthpragada@gmail.com>
2019-07-09 08:00:26 +0530

    09-JUL

--- oop/course.py
+++ oop/course.py
@@ -1,29 +1,43 @@
 class Course:
+    # Class attribute or static variable
+    taxrate = 14
+
+    @staticmethod
+    def change_taxrate(newtaxrate):
+        Course.taxrate = newtaxrate
+
     # Constructor
     def __init__(self, title, duration=40, fee=4000):
         # Data attributes
         self.title = title
         self.duration = duration
-        self.__fee = fee  # Private attribute
+        self.fee = fee  # Private attribute
+
+    def get_fee(self):
+        return self.fee + (self.fee * Course.taxrate / 100)
 
     # Methods
     def print_details(self):
         print(f"Title     : {self.title}")
         print(f"Duration  : {self.duration}")
-        print(f"Fee       : {self.__fee}")
+        print(f"Fee       : {self.fee}")
 
     def change_fee(self, newfee):
         if newfee >= 0:
-            self.__fee = newfee
+            self.fee = newfee
         else:
             raise ValueError(f"Invalid Fee {newfee}. Fee cannot be negative!")
 
 
-c1 = Course("Python", 40, 4000)  # Object
-# c1.fee = 5000   # new attribute fee is created
-# c1._Course__fee = 5000
-c1.change_fee(-5000)
-c1.print_details()
+if __name__ == '__main__':
+    Course.change_taxrate(18)
 
-c2 = Course("Java EE", fee=5000)
-c2.print_details()
+    c1 = Course("Python", 40, 4000)  # Object
+    print(c1.get_fee())
+    # c1.fee = 5000   # new attribute fee is created
+    # c1._Course__fee = 5000
+    # c1.change_fee(-5000)
+    # c1.print_details()
+    #
+    # c2 = Course("Java EE", fee=5000)
+    # c2.print_details()
--- oop/person.py
+++ oop/person.py
@@ -3,9 +3,18 @@ class Person:
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return f"{self.name} - {self.age}"
+
+    def __eq__(self, other):
+        print("__eq__")
+        return self.name == other.name and self.age == other.age
+
 
 p1 = Person('Bill Gates', 63)
 p2 = Person('Bill Gates', 63)
 
-print(p1 == p2)
+print(p1 == p2)  # p1.__eq__(p2)
 print(p1)
+
+print(p1 > p2)
